53fe19558f8c7f1676f8883877fe5a6cf7d94f09
Emre Güler <guler.emre.99@gmail.com>
2024-02-16 14:02:54 +0300

    .

--- AuditorApp/Controllers/EmployeeController.cs
+++ AuditorApp/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
-﻿using AuditorApp.ViewModels;
+﻿using AuditorApp.Models;
+using AuditorApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.SqlClient;
 
 namespace AuditorApp.Controllers
@@ -26,7 +28,91 @@ namespace AuditorApp.Controllers
                     AuditorLastName = (string)dr["AuditorLastName"]
                 });
             }
+            conn.Close();
+
             return View(list);
+        }
+        public IActionResult Add() 
+        {
+            List<SelectListItem> factories = new List<SelectListItem>();
+            SqlConnection conn = Db.Conn();
+            SqlCommand cmd = new SqlCommand("SELECT* FROM Factories WHERE IsDeleted = 0", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                factories.Add(new SelectListItem
+                {
+                    Value = dr["Id"].ToString(),
+                    Text = dr["Name"].ToString(),
+                });
+            }
+            dr.Close();
+
+
+            List<SelectListItem> nameAuditors = new List<SelectListItem>();
+            List<SelectListItem> lastNameAuditors = new List<SelectListItem>();
+            SqlCommand cmdAuditor = new SqlCommand("SELECT * FROM Auditors WHERE IsDeleted = 0", conn);
+            SqlDataReader drAuditor = cmdAuditor.ExecuteReader();
+            while (drAuditor.Read())
+            {
+                nameAuditors.Add(new SelectListItem{
+                    Value = dr["Id"].ToString(),
+                    Text = dr["Name"].ToString(),
+                });
+            }
+            dr.Close();
+            while (drAuditor.Read())
+            {
+                lastNameAuditors.Add(new SelectListItem{
+                    Value = dr["Id"].ToString(),
+                    Text = dr["LastName"].ToString(),
+                });
+            }
+            dr.Close();
+
+
+            List<SelectListItem> departmants = new List<SelectListItem>();
+            SqlCommand cmdDepartmant = new SqlCommand("SELECT * FROM Departmants WHERE IsDeleted = 0", conn);
+            SqlDataReader drDepartmant = cmdDepartmant.ExecuteReader();
+            while (drDepartmant.Read())
+            {
+                departmants.Add(new SelectListItem{ 
+                    Value = dr["Id"].ToString(),
+                    Text = dr["Name"].ToString(),
+                });
+            }
+            conn.Close();
+
+            ViewData["factories"] = factories;
+            ViewData["nameAuditors"] = nameAuditors;
+            ViewData["lastNameAuditors"] = lastNameAuditors;
+            ViewData["departmants"] = departmants;
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Add(Employee employee, List<int> departmants)
+        {
+            SqlConnection conn = Db.Conn();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Employees (Name, LastName, Point, FactoryId, AuditorId) OUTPUT inserted.id VALUES (@name, @lastName, @point, @factoryId, @auditorId)", conn);
+            cmd.Parameters.AddWithValue("@name", employee.Name);
+            cmd.Parameters.AddWithValue("@lastName", employee.LastName);
+            cmd.Parameters.AddWithValue("@point", employee.Point);
+            cmd.Parameters.AddWithValue("@factoryId", employee.FactoryId);
+            cmd.Parameters.AddWithValue("@auditorId", employee.AuditorId);
+            conn.Open();
+            int lastId = (int)cmd.ExecuteScalar();
+            foreach (int i in departmants)
+            {
+                cmd = new SqlCommand("INSERT INTO EmployeeDepartmantRel VALUES (@employeeId, @departmantId", conn);
+                cmd.Parameters.AddWithValue("@employeeId", lastId);
+                cmd.Parameters.AddWithValue("@departmantId", i);
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+
+            return RedirectToAction("Index");
         }
     }
 }
